073e199226d6bd0deec5c1e4b064d730d2facd44
Carlos Vallory <carlvallory@gmail.com>
2026-02-13 19:14:37 -0300

    Dashboard Financiero fix route error

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use CarlVallory\KrayinFinancialReports\Http\Controllers\FinancialReportController;
 
-Route::group(['middleware' => ['web', 'admin'], 'prefix' => 'admin/financial-reports'], function () {
+Route::group(['middleware' => ['web', 'admin_locale', 'user'], 'prefix' => 'admin/financial-reports'], function () {
     Route::get('/', [FinancialReportController::class, 'index'])->name('admin.financial_reports.index');
 });
