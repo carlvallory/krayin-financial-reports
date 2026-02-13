048de91160269c87d8fefced4da729eb9937dc29
Carlos Vallory <carlvallory@gmail.com>
2026-02-13 19:12:02 -0300

    Dashboard Financiero -menu

---
+++ src/Config/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    [
+        'key'        => 'financial_reports',
+        'name'       => 'Informes',
+        'route'      => 'admin.financial_reports.index',
+        'sort'       => 2,
+        'icon-class' => 'icon-sales', // Using icon-sales or similar, or dashboard
+    ]
+];
--- src/Providers/KrayinFinancialReportsServiceProvider.php
+++ src/Providers/KrayinFinancialReportsServiceProvider.php
@@ -18,16 +18,6 @@ class KrayinFinancialReportsServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'krayin-financial-reports');
 
         $this->loadRoutesFrom(__DIR__ . '/../Http/routes.php');
-
-        Event::listen('core.menu.make', function ($menu) {
-            $menu->add([
-                'key'        => 'financial_reports',
-                'name'       => 'Informes',
-                'route'      => 'admin.financial_reports.index',
-                'sort'       => 2,
-                'icon-class' => 'icon-sales',
-            ]);
-        });
     }
 
     /**
@@ -37,5 +27,8 @@ class KrayinFinancialReportsServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../Config/menu.php', 'menu.admin'
+        );
     }
 }
