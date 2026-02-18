631bad72a62b2a4c44424241cff7f4bc8b9abc13
Carlos Vallory <carlvallory@gmail.com>
2026-02-18 09:47:22 -0300

    Add debug fix 2

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use CarlVallory\KrayinFinancialReports\Http\Controllers\FinancialReportController;
 
 Route::group(['middleware' => ['web', 'admin_locale', 'user'], 'prefix' => 'admin/financial-reports'], function () {
-    Route::get('/', [FinancialReportController::class, 'index'])->name('admin.financial_reports.index');
+    Route::get('', [FinancialReportController::class, 'index'])->name('krayin.financial-reports.index');
+    Route::get('configure', [FinancialReportController::class, 'configure'])->name('krayin.financial-reports.configure');
+    Route::post('configure', [FinancialReportController::class, 'storeConfiguration'])->name('krayin.financial-reports.configure.store');
 });
